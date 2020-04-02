f2389b0e31a69f3fe3dbd432485c82251d1c50a9
draw-de <57506579+draw-de@users.noreply.github.com>
2020-04-02 04:04:13 -0400

    README is able to grab GitHub API

---
+++ README.md
@@ -0,0 +1,17 @@
+(https://avatars2.githubusercontent.com/u/57506579?v=4 "draw-de's Avatar Picture)
+        
+        #a
+        
+        ##Description
+        a
+        
+        ##Installation
+        a
+        
+        ##Usage
+        a
+      
+        ##Issues
+        If there's any issues with the application, please contact [draw-de](https://api.github.com/users/draw-de) or make an issue ticket
+        
+        ######undefined}
--- index.js
+++ index.js
@@ -1,74 +1,93 @@
 const fs = require("fs");
 const inquirer = require("inquirer");
 const util = require("util");
+const axios = require("axios");
 
 const writeFileAsync = util.promisify(fs.writeFile);
 
-function promptUser() {
-  return inquirer
-    .prompt([
-      {
-        type: "input",
-        name: "github",
-        message: "Enter your GitHub username."
-      },
-      {
-        type: "input",
-        name: "title",
-        message: "Type in the title of your new project."
-      },
-      {
-        type: "input",
-        name: "description",
-        message: "Type in your description of your project."
-      },
-      {
-        type: "input",
-        name: "installation",
-        message: "Type in how to install your project."
-      },
-      {
-        type: "input",
-        name: "usage",
-        message: "Explain how you would use the project."
-      },
-      {
-        type: "list",
-        name: "license",
-        choices: [
-          "Apache License 2.0",
-          "GNU General Public License v3.0",
-          "MIT License",
-          "None"
-        ]
+inquirer
+  .prompt([
+    {
+      type: "input",
+      name: "github",
+      message: "Enter your GitHub username."
+    }
+  ])
+  .then(function(git) {
+    const githubURL = `https://api.github.com/users/${git.github}`;
+
+    axios({
+      method: "get",
+      url: githubURL
+    }).then(function(obj) {
+      // console.log(obj.data);
+
+      function promptUser() {
+        return inquirer.prompt([
+          {
+            type: "input",
+            name: "title",
+            message: "Type in the title of your new project."
+          },
+          {
+            type: "input",
+            name: "description",
+            message: "Type in your description of your project."
+          },
+          {
+            type: "input",
+            name: "installation",
+            message: "Type in how to install your project."
+          },
+          {
+            type: "input",
+            name: "usage",
+            message: "Explain how you would use the project."
+          },
+          {
+            type: "list",
+            name: "license",
+            choices: [
+              "Apache License 2.0",
+              "GNU General Public License v3.0",
+              "MIT License",
+              "None"
+            ]
+          }
+        ]);
       }
-    ])
-    .then(function(answers) {
-      const githubURL = "https://api.github.com";
+
+      function generateREADME(answers) {
+        return `(${obj.data.avatar_url} "${obj.data.login}'s Avatar Picture)
+        
+        #${answers.title}
+        
+        ##Description
+        ${answers.description}
+        
+        ##Installation
+        ${answers.installation}
+        
+        ##Usage
+        ${answers.usage}
+      
+        ##Issues
+        If there's any issues with the application, please contact [${obj.data.login}](${obj.data.url}) or make an issue ticket
+        
+        ######${answers.license}}`;
+      }
+
+      promptUser()
+        .then(function(answers) {
+          const readme = generateREADME(answers);
+
+          return writeFileAsync("README.md", readme);
+        })
+        .then(function() {
+          console.log("Successfully wrote to README.md");
+        })
+        .catch(function(err) {
+          console.log(err);
+        });
     });
-}
-
-function generateREADME(answers) {
-  return `#${answers.title}
-
-  ${answers.description}
-  
-  ${answers.installation}
-  
-  ${answers.usage}
-  
-  ######${answers.license}`;
-}
-
-promptUser()
-  .then(function(answers) {
-    const readme = generateREADME(answers);
-
-    return writeFileAsync("README.md", readme);
-  })
-  .then(function() {
-    console.log("Successfully wrote to README.md");
-  })
-  .catch(function(err) {
-    console.log(err);
   });
